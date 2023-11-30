e83b6d694cfd7ab6b6f35e2f5c83881838710d17
fromofca <101668135+fromofca@users.noreply.github.com>
2023-11-29 22:58:17 -0300

    Update frt.py

--- frt.py
+++ frt.py
@@ -54,9 +54,10 @@ def glob(tipo):
     for i, linha in enumerate(linhas):
         tamanho = len(linha)
         metade = tamanho // len(cores)
+        espacos = ' ' * i
         for j, cor in enumerate(cores):
             parte = linha[j * metade : (j + 1) * metade]
-            print(f'{cor}{parte}', end='')
+            print(f'{espacos}{cor}{parte}', end='')
         print()
         
 import time,subprocess
